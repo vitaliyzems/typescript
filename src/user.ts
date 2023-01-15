a51d15e23ecf88fa97164cce58800f7e3050dc77
Vitaliy <vitalyazems@gmail.com>
2023-01-15 22:12:56 +0600

    Выполнил дз

---
+++ src/user.ts
@@ -0,0 +1,27 @@
+import { renderBlock } from './lib.js';
+
+export function renderUserBlock(
+  userName: string,
+  avatarLink: string,
+  favoriteItemsAmount: number
+) {
+  const favoritesCaption = favoriteItemsAmount
+    ? favoriteItemsAmount
+    : 'ничего нет';
+  const hasFavoriteItems = favoriteItemsAmount ? true : false;
+
+  renderBlock(
+    'user-block',
+    `
+    <div class="header-container">
+      <img class="avatar" src=${avatarLink} alt="Wade Warren" />
+      <div class="info">
+          <p class="name">${userName}</p>
+          <p class="fav">
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  );
+}
